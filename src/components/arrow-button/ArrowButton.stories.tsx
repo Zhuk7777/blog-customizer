ce48d7e4a1173c30a65118f04495e8910ea49a4c
David Zhukov <david2001off@mail.ru>
2024-10-19 04:46:50 +0300

    Blog customizer

--- src/components/arrow-button/ArrowButton.stories.tsx
+++ src/components/arrow-button/ArrowButton.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { ArrowButton } from './ArrowButton';
+import { useDisclosure } from 'src/hooks/useDisclosure';
 
 const meta: Meta<typeof ArrowButton> = {
 	component: ArrowButton,
@@ -9,11 +10,16 @@ const meta: Meta<typeof ArrowButton> = {
 export default meta;
 type Story = StoryObj<typeof ArrowButton>;
 
+const ArrowButtonWithState = () => {
+	const { isOpen, toggle } = useDisclosure();
+	return <ArrowButton onClick={toggle} isOpen={isOpen} />;
+};
+
 export const ArrowButtonStory: Story = {
 	render: () => {
 		return (
 			<>
-				<ArrowButton />
+				<ArrowButtonWithState />
 			</>
 		);
 	},
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,13 +2,126 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import {
+	ArticleStateType,
+	OptionType,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
+import { useDisclosure } from 'src/hooks/useDisclosure';
+import { FormEvent, useCallback, useEffect, useRef, useState } from 'react';
+import clsx from 'clsx';
+import { Text } from '../text';
+import { Select } from '../select';
+import { RadioGroup } from '../radio-group';
+import { Separator } from '../separator';
 
-export const ArticleParamsForm = () => {
+export const ArticleParamsForm = ({
+	updateArticleSettings,
+}: {
+	updateArticleSettings: (settings: ArticleStateType) => void;
+}) => {
+	const { isOpen, toggle, close } = useDisclosure();
+	const sideBarRef = useRef<HTMLDivElement | null>(null);
+
+	const [settings, setSettings] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	useEffect(() => {
+		const handleClickOutside = (evt: MouseEvent) => {
+			if (
+				sideBarRef.current &&
+				!sideBarRef.current.contains(evt.target as Node)
+			) {
+				close();
+			}
+		};
+
+		document.addEventListener('mousedown', handleClickOutside);
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [close, sideBarRef]);
+
+	const handleChange = useCallback(
+		(field: keyof ArticleStateType, value: OptionType) => {
+			setSettings((prevSettings) => ({
+				...prevSettings,
+				[field]: value,
+			}));
+		},
+		[]
+	);
+
+	const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+		evt.preventDefault();
+		updateArticleSettings(settings);
+	};
+
+	const handleReset = () => {
+		setSettings(defaultArticleState);
+		updateArticleSettings(defaultArticleState);
+	};
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton onClick={toggle} isOpen={isOpen} />
+			<aside
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}
+				ref={sideBarRef}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmit}
+					onReset={handleReset}>
+					<Text as='h2' size={31} uppercase weight={800}>
+						Задайте параметры
+					</Text>
+					<Select
+						title='Шрифт'
+						options={fontFamilyOptions}
+						selected={settings.fontFamilyOption}
+						onChange={(selectedFontFamily) =>
+							handleChange('fontFamilyOption', selectedFontFamily)
+						}
+					/>
+					<RadioGroup
+						title='Размер шрифта'
+						name='fontSize'
+						options={fontSizeOptions}
+						selected={settings.fontSizeOption}
+						onChange={(selectedFontSize) =>
+							handleChange('fontSizeOption', selectedFontSize)
+						}
+					/>
+					<Select
+						title='Цвет шрифта'
+						options={fontColors}
+						selected={settings.fontColor}
+						onChange={(selectedFontColor) =>
+							handleChange('fontColor', selectedFontColor)
+						}
+					/>
+					<Separator />
+					<Select
+						title='Цвет фона'
+						options={backgroundColors}
+						selected={settings.backgroundColor}
+						onChange={(selectedBgColor) =>
+							handleChange('backgroundColor', selectedBgColor)
+						}
+					/>
+					<Select
+						title='Ширина контента'
+						options={contentWidthArr}
+						selected={settings.contentWidth}
+						onChange={(selectedContentWidth) =>
+							handleChange('contentWidth', selectedContentWidth)
+						}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
